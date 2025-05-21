d6d822f499ff51678c605adf6e856cee9b415b65
atmaca06-dev <s.ali.atmaca@outlook.com>
2025-05-21 21:10:43 +0300

    Update general_scraper.py

--- general_scraper.py
+++ general_scraper.py
@@ -1,3 +1,12 @@
-def scrape_site(command):
-    # Basit demo: komutu döndür
-    return 'Veri çekildi: ' + command
+def scrape_site(site, query):
+    # site = "hepsiemlak", "sahibinden" gibi
+    # query = "Ankara Sincan satılık" gibi
+    # Siteye göre yönlendirme yap veya işle
+    if site == "hepsiemlak":
+        # hepsiemlak işlemleri...
+        pass
+    elif site == "sahibinden":
+        # sahibinden işlemleri...
+        pass
+    else:
+        return "Desteklenmeyen site"
