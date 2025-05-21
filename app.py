1edf131ba22192517c25cf71e4362fc286125f54
atmaca06-dev <s.ali.atmaca@outlook.com>
2025-05-21 06:11:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def webhook():
         to=from_number
     )
 
-   if __name__ == "__main__":
-       app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
